25f1538059628b51c913a883a30fed042a2f1a5d
mahamudul310 <hmahamudul480@gmail.com>
2018-07-29 11:31:41 +0600

    Attacth plugin menu and submenu onto plugin

--- wp-my-books.php
+++ wp-my-books.php
@@ -20,6 +20,7 @@ if(!defined("MY_BOOK_PLUGIN_URL"))
     define ("MY_BOOK_PLUGIN_URL", plugins_url()."/my-books");
 
 function my_book_include_assets(){
+    // All Style 
     wp_enqueue_style("bootstrap", MY_BOOK_PLUGIN_URL."/assets/css/bootstrap.min.css",'');
     wp_enqueue_style("datatable", MY_BOOK_PLUGIN_URL."/assets/css/jquery.dataTables.min.css",'');
     wp_enqueue_style("notifybar", MY_BOOK_PLUGIN_URL."/assets/css/jquery.notifyBar.css",'');
@@ -37,3 +38,18 @@ function my_book_include_assets(){
 }
 
 add_action("init","my_book_include_assets");
+
+function my_book_plugin_menus(){
+    add_menu_page("My Book","My Book","manage_options","book-list","my_book_list","dashicons-book-alt",30);
+    add_submenu_page("book-list","Book List","Book List","manage_options","book-list","my_book_list");
+    add_submenu_page("book-list","Add New","Add New","manage_options","add-new","my_book_add");
+}
+add_action("admin_menu","my_book_plugin_menus");
+
+function my_book_list() {
+    include_once MY_BOOK_PLUGIN_DIR_PATH."/views/book-list.php";
+}
+
+function my_book_add() {
+    include_once MY_BOOK_PLUGIN_DIR_PATH."/views/book-add.php";
+}
