4bab2ee8ca115501b65a80d0dc415ecd66e965c1
mahamudul310 <hmahamudul480@gmail.com>
2018-07-29 10:58:57 +0600

    Assets library file setup includeing onto plugin

--- wp-my-books.php
+++ wp-my-books.php
@@ -19,7 +19,21 @@ if(!defined("MY_BOOK_PLUGIN_DIR_PATH"))
 if(!defined("MY_BOOK_PLUGIN_URL"))
     define ("MY_BOOK_PLUGIN_URL", plugins_url()."/my-books");
 
+function my_book_include_assets(){
+    wp_enqueue_style("bootstrap", MY_BOOK_PLUGIN_URL."/assets/css/bootstrap.min.css",'');
+    wp_enqueue_style("datatable", MY_BOOK_PLUGIN_URL."/assets/css/jquery.dataTables.min.css",'');
+    wp_enqueue_style("notifybar", MY_BOOK_PLUGIN_URL."/assets/css/jquery.notifyBar.css",'');
+    wp_enqueue_style("style", MY_BOOK_PLUGIN_URL."/assets/css/style.css",'');
+    
+    //Scripts
+    wp_enqueue_script("jquery");
+    wp_enqueue_script("bootstrap.min.js", MY_BOOK_PLUGIN_URL."/assets/js/bootstrap.min.js","", true);
+    wp_enqueue_script("validation.min.js", MY_BOOK_PLUGIN_URL."/assets/js/jquery.validate.min.js","", true);
+    wp_enqueue_script("datatable.min.js", MY_BOOK_PLUGIN_URL."/assets/js/jquery.dataTables.min.js","", true);
+    wp_enqueue_script("jquery.min.js", MY_BOOK_PLUGIN_URL."/assets/js/jquery.min.js","", true);
+    wp_enqueue_script("jquery.notifyBar.js", MY_BOOK_PLUGIN_URL."/assets/js/jquery.notifyBar.js","", true);
+    wp_enqueue_script("script.js", MY_BOOK_PLUGIN_URL."/assets/js/script.js","", true);
+    wp_localize_script("script.js","mybookajaxurl",admin_url("admin-ajax.php"));
+}
 
-echo "<h1>",  MY_BOOK_PLUGIN_DIR_PATH; echo "</h1>";
- echo '<br/>';
-echo "<h1>",  MY_BOOK_PLUGIN_URL; echo "</h1>";
+add_action("init","my_book_include_assets");
